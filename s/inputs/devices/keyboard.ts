84e62bd572f54231713b864bf8f6ba6738fc93b9
Chase Moskal <chasemoskal@gmail.com>
2025-08-30 14:46:06 -0700

    fix: keyboard device

--- s/inputs/devices/keyboard.ts
+++ s/inputs/devices/keyboard.ts
@@ -1,24 +1,44 @@
 
-import {ev} from "@e280/stz"
+import {ev, sub} from "@e280/stz"
+import {Sample} from "../types.js"
+import {Device} from "../parts/device.js"
 import {modprefix} from "../utils/modprefix.js"
-import {SamplerDevice} from "../parts/device.js"
 
-export class KeyboardDevice extends SamplerDevice {
+export class KeyboardDevice extends Device {
+	on = sub<Sample>()
 	dispose: () => void
+	#held = new Set<string>()
 
 	constructor(target: EventTarget) {
 		super()
 
-		const dispatch = (event: KeyboardEvent, value: number) => {
-			const {code} = event
-			this.setSample(code, value)
-			this.setSample(modprefix(event, code), value)
+		const down = (code: string) => {
+			this.#held.add(code)
+			this.on.pub(code, 1)
+		}
+
+		const up = (code: string) => {
+			this.#held.delete(code)
+			this.on.pub(code, 0)
 		}
 
 		this.dispose = ev(target, {
-			keydown: (event: KeyboardEvent) => dispatch(event, 1),
-			keyup: (event: KeyboardEvent) => dispatch(event, 0),
+			keydown: (event: KeyboardEvent) => {
+				if (event.repeat) return
+				down(event.code)
+				down(modprefix(event, event.code))
+			},
+
+			keyup: (event: KeyboardEvent) => {
+				up(event.code)
+				up(modprefix(event, event.code))
+			},
 		})
+	}
+
+	takeSamples() {
+		return [...this.#held]
+			.map(code => [code, 1] as Sample)
 	}
 }
 
