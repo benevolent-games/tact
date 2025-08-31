38094c9efe259929097b56b4b78f7b70b006c522
Chase Moskal <chasemoskal@gmail.com>
2025-08-31 02:49:01 -0700

    rename: inputs2 to station

--- s/tests.test.ts
+++ s/tests.test.ts
@@ -1,7 +1,7 @@
 
 import {Science} from "@e280/science"
 import inputs from "./inputs/inputs.test.js"
-import inputs2 from "./inputs2/inputs.test.js"
+import inputs2 from "./station/inputs.test.js"
 
 await Science.run({
 	inputs,
