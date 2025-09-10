3d3d80c255acb6b6fbd3df91af02ae8d6b8143be
Chase Moskal <chasemoskal@gmail.com>
2025-09-09 17:06:08 -0700

    feat: loaded bindings are merged into good ones

--- s/core/deck/parts/bindings-depot.ts
+++ s/core/deck/parts/bindings-depot.ts
@@ -1,7 +1,8 @@
 
 import {Kv} from "@e280/kv"
-import {deep, range} from "@e280/stz"
+import {range} from "@e280/stz"
 import {Bindings} from "../../bindings/types.js"
+import {mergeBindings} from "./merge-bindings.js"
 
 export class BindingsDepot<B extends Bindings> {
 	#kv: Kv<B>
@@ -11,14 +12,14 @@ export class BindingsDepot<B extends Bindings> {
 			rootKv: Kv,
 			public readonly fallbackBindings: B,
 		) {
-		this.#kv = rootKv.scope("bindings")
+		this.#kv = rootKv.scope<B>("bindings")
 	}
 
 	async loadAll() {
 		const indices = range(this.portCount)
 		const keys = indices.map(k => k.toString())
 		return (await this.#kv.gets(...keys))
-			.map(bindings => bindings ?? deep.clone(this.fallbackBindings)) as B[]
+			.map(suspect => mergeBindings(this.fallbackBindings, suspect))
 	}
 
 	async save(index: number, bindings: B) {
---
+++ s/core/deck/parts/merge-bindings.ts
@@ -0,0 +1,21 @@
+
+import {deep} from "@e280/stz"
+import {Bindings} from "../../bindings/types.js"
+
+export function mergeBindings<B extends Bindings>(normative: B, sus?: B) {
+	const bindings = deep.clone(normative)
+
+	if (!sus)
+		return bindings
+
+	for (const [mode, bracket] of Object.entries(bindings)) {
+		for (const actionKey of Object.keys(bracket)) {
+			const susAction = sus[mode]?.[actionKey]
+			if (susAction)
+				bracket[actionKey] = susAction
+		}
+	}
+
+	return bindings
+}
+
