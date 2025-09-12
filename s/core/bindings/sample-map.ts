d0bd973bdf202054d91835d7c2d86527b2a9f3e3
Chase Moskal <chasemoskal@gmail.com>
2025-09-12 16:52:18 -0700

    tidy: sample map

--- s/core/bindings/sample-map.ts
+++ s/core/bindings/sample-map.ts
@@ -3,18 +3,12 @@ import {MapG} from "@e280/stz"
 import {Sample} from "../devices/types.js"
 
 export class SampleMap extends MapG<string, number> {
-	static merge(...maps: SampleMap[]) {
-		const samples = new this()
-		for (const map of maps) {
-			for (const sample of map)
-				samples.mergeSample(sample)
-		}
-		return samples
-	}
-
 	constructor(samples?: Iterable<Sample>) {
 		super()
-		if (samples) this.setSamples(samples)
+		if (samples) {
+			for (const [code, value] of samples)
+				this.set(code, value)
+		}
 	}
 
 	zero() {
@@ -22,27 +16,9 @@ export class SampleMap extends MapG<string, number> {
 		return this
 	}
 
-	setSamples(samples: Iterable<Sample>) {
-		for (const [code, value] of samples)
-			this.set(code, value)
-	}
-
 	mergeSample([code, value]: Sample) {
 		const previous = this.get(code) ?? 0
 		if (value > previous) this.set(code, value)
-		return this
-	}
-
-	mergeSamples(samples: Iterable<Sample>) {
-		for (const sample of samples) this.mergeSample(sample)
-		return this
-	}
-
-	mergeMaps(...maps: SampleMap[]) {
-		for (const map of maps) {
-			for (const sample of map)
-				this.mergeSample(sample)
-		}
 		return this
 	}
 }
