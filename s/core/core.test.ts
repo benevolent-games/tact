f36f631405a7f336c939b88c538a531285d6a63a
Chase Moskal <chasemoskal@gmail.com>
2026-03-14 01:23:08 -0700

    improve: tests

--- s/core/core.test.ts
+++ s/core/core.test.ts
@@ -6,16 +6,16 @@ import {testPlug, testSetupAlpha, testSetupBravo} from "./testing/testing.js"
 
 export default Science.suite({
 	"sample to action value": test(async() => {
-		const {time, device, resolve} = testSetupAlpha()
+		const {clock, device, resolve} = testSetupAlpha()
 		{
-			time.frame = 1
+			clock.frame = 1
 			device.setSample("Space", 1)
 			const actions = resolve()
 			expect(actions.basic.jump.value).is(1)
 			expect(actions.basic.shoot.value).is(0)
 		}
 		{
-			time.frame = 2
+			clock.frame = 2
 			device.setSample("Space", 2)
 			device.setSample("pointer.button.left", 3)
 			const actions = resolve()
@@ -25,96 +25,113 @@ export default Science.suite({
 	}),
 
 	"hold timing": test(async() => {
-		const {time, device, resolve} = testSetupAlpha()
-		{
-			time.frame = 1
-			device.setSample("KeyG", 0)
-			const actions = resolve()
-			expect(actions.basic.grenade.value).is(0)
+		const {clock, device, resolve, actions} = testSetupAlpha()
+		const proceed = (options: {time: number, value: number}) => {
+			clock.time = options.time
+			device.setSample("KeyG", options.value)
+			resolve()
 		}
-		{
-			time.frame = 2
-			device.setSample("KeyG", 1)
-			const actions = resolve()
-			expect(actions.basic.grenade.value).is(0)
+
+		proceed({time: 0, value: 0})
+		expect(actions.basic.grenade.value).is(0)
+
+		proceed({time: 10, value: 1})
+		expect(actions.basic.grenade.value).is(0)
+
+		proceed({time: 210, value: 1})
+		expect(actions.basic.grenade.value).is(1)
+	}),
+
+	"hold timing onDown": test(async() => {
+		const {clock, device, resolve, actions} = testSetupAlpha()
+		const proceed = (options: {time: number, value: number}) => {
+			clock.time = options.time
+			device.setSample("KeyG", options.value)
+			resolve()
 		}
-		{
-			time.frame = 15
-			device.setSample("KeyG", 1)
-			const actions = resolve()
-			expect(actions.basic.grenade.value).is(1)
-		}
+
+		let count = 0
+		actions.basic.grenade.onDown(() => { count++ })
+
+		proceed({time: 0, value: 0})
+		expect(count).is(0)
+
+		proceed({time: 10, value: 1})
+		expect(count).is(0)
+
+		proceed({time: 210, value: 1})
+		expect(count).is(1)
 	}),
 
 	"hub": Science.suite({
 		"device inputs work": test(async() => {
-			const {hub, time} = testSetupBravo()
+			const {hub, clock} = testSetupBravo()
 			const d1 = testPlug(hub, new SamplerDevice())
 			d1.setSample("Space", 1)
-			const [p1, p2] = hub.poll(time.now)
+			const [p1, p2] = hub.poll(clock.time)
 			expect(p1.actions.basic.jump.value).is(1)
 			expect(p2.actions.basic.jump.value).is(0)
 		}),
 
 		"reveal overlay works": test(async() => {
-			const {hub, time} = testSetupBravo()
+			const {hub, clock} = testSetupBravo()
 			const d1 = testPlug(hub, new SamplerDevice())
 			d1.setSample("Backslash", 1)
-			hub.poll(time.now)
+			hub.poll(clock.time)
 			expect(hub.metaPort.actions[metaMode].revealOverlay.value)
 				.is(1)
 		}),
 
 		"two devices playing on separate ports": test(async() => {
-			const {hub, time} = testSetupBravo()
+			const {hub, clock} = testSetupBravo()
 			const c1 = testPlug(hub, new SamplerDevice())
 			const c2 = testPlug(hub, new SamplerDevice())
 			c1.setSample("Space", 1)
 			c2.setSample("Space", 2)
-			const [p1, p2] = hub.poll(time.now)
+			const [p1, p2] = hub.poll(clock.time)
 			expect(p1.actions.basic.jump.value).is(1)
 			expect(p2.actions.basic.jump.value).is(2)
 		}),
 
 		"device can shimmy": test(async() => {
-			const {hub, time} = testSetupBravo()
+			const {hub, clock} = testSetupBravo()
 			const c1 = testPlug(hub, new SamplerDevice())
 			hub.shimmy(c1, 1)
 			c1.setSample("Space", 1)
-			const [p1, p2] = hub.poll(time.now)
+			const [p1, p2] = hub.poll(clock.time)
 			expect(p1.actions.basic.jump.value).is(0)
 			expect(p2.actions.basic.jump.value).is(1)
 		}),
 
 		"two devices can share a port": test(async() => {
-			const {hub, time} = testSetupBravo()
+			const {hub, clock} = testSetupBravo()
 			const c1 = testPlug(hub, new SamplerDevice())
 			const c2 = testPlug(hub, new SamplerDevice())
 			hub.shimmy(c2, -1)
 			expect(hub.portByDevice(c1)).is(hub.portByDevice(c2))
 			{
 				c1.setSample("Space", 1)
-				const [p1, p2] = hub.poll(time.now)
+				const [p1, p2] = hub.poll(clock.time)
 				expect(p1.actions.basic.jump.value).is(1)
 				expect(p2.actions.basic.jump.value).is(0)
 			}
 			{
 				c2.setSample("Space", 1)
-				const [p1, p2] = hub.poll(time.now)
+				const [p1, p2] = hub.poll(clock.time)
 				expect(p1.actions.basic.jump.value).is(1)
 				expect(p2.actions.basic.jump.value).is(0)
 			}
 			{
 				c1.setSample("Space", 1)
 				c2.setSample("Space", 2)
-				const [p1, p2] = hub.poll(time.now)
+				const [p1, p2] = hub.poll(clock.time)
 				expect(p1.actions.basic.jump.value).is(2)
 				expect(p2.actions.basic.jump.value).is(0)
 			}
 			{
 				c1.setSample("Space", 2)
 				c2.setSample("Space", 1)
-				const [p1, p2] = hub.poll(time.now)
+				const [p1, p2] = hub.poll(clock.time)
 				expect(p1.actions.basic.jump.value).is(2)
 				expect(p2.actions.basic.jump.value).is(0)
 			}
--- s/core/testing/testing.ts
+++ s/core/testing/testing.ts
@@ -2,16 +2,16 @@
 import {Hub} from "../hub/hub.js"
 import {Port} from "../hub/port.js"
 import {Device} from "../devices/device.js"
-import {asBindings, Code} from "../bindings/types.js"
 import {Resolver} from "../bindings/resolver.js"
 import {SampleMap} from "../bindings/sample-map.js"
+import {asBindings, Code} from "../bindings/types.js"
 import {SamplerDevice} from "../devices/infra/sampler.js"
 
-export class TestTime {
-	frame = 0
+export class TestClock {
+	time = 0
 
-	get now() {
-		return (this.frame++) * (1000 / 60)
+	set frame(frame: number) {
+		this.time = frame * (1000 / 60)
 	}
 }
 
@@ -31,26 +31,27 @@ export function testPlug<C extends Device>(hub: Hub<any>, device: C) {
 }
 
 export function testSetupAlpha() {
-	const time = new TestTime()
+	const clock = new TestClock()
 	const device = new SamplerDevice()
 	const resolver = new Resolver(testBindings())
 	const modes = new Set(Object.keys(resolver.bindings))
+	const actions = resolver.actions
 	const resolve = () => resolver.resolve(
-		time.now,
+		clock.time,
 		modes as any,
 		new SampleMap(device.samples())
 	)
-	return {device, resolver, resolve, time}
+	return {device, resolver, resolve, clock, actions}
 }
 
 export function testSetupBravo() {
-	const time = new TestTime()
+	const clock = new TestClock()
 	const port = () => {
 		const port = new Port(testBindings())
 		port.modes.adds("basic")
 		return port
 	}
 	const hub = new Hub([port(), port(), port(), port()])
-	return {hub, time}
+	return {hub, clock}
 }
 
