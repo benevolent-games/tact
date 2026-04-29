ad85d2b4b7f464fa4d4fe8b903437f079c85faa8
Chase Moskal <chasemoskal@gmail.com>
2026-04-28 17:14:27 -0700

    add: bravo device

---
+++ s/bravo/device/pointer.ts
@@ -0,0 +1,97 @@
+
+import {Vec2} from "@benev/math"
+import {disposer, ev} from "@e280/stz"
+import {SamplerDevice} from "./sampler.js"
+import {splitAxis} from "../utils/split-axis.js"
+
+export class PointerDevice extends SamplerDevice {
+	client = new Vec2(0, 0)
+	movement = new Vec2(0, 0)
+	dispose = disposer()
+
+	constructor(target: EventTarget = window) {
+		super()
+
+		this.dispose.schedule(ev(target, {
+			pointerdown: (event: PointerEvent) => {
+				const code = PointerDevice.buttonCode(event)
+				this.setSample(code, 1)
+			},
+
+			pointerup: (event: PointerEvent) => {
+				const code = PointerDevice.buttonCode(event)
+				this.setSample(code, 0)
+			},
+
+			pointermove: (event: PointerEvent) => {
+				this.client.x = event.clientX
+				this.client.y = event.clientY
+				this.movement.x += event.movementX
+				this.movement.y += event.movementY
+			},
+
+			wheel: (event: WheelEvent) => {
+				this.sampleMap.set("pointer.wheel.right", 0)
+				this.sampleMap.set("pointer.wheel.left", 0)
+				this.sampleMap.set("pointer.wheel.up", 0)
+				this.sampleMap.set("pointer.wheel.down", 0)
+				for (const [code, value] of PointerDevice.wheelCodes(event))
+					this.setSample(code, value)
+			},
+		}))
+	}
+
+	static buttonCode(event: PointerEvent) {
+		switch (event.button) {
+			case 0: return "pointer.button.left"
+			case 1: return "pointer.button.middle"
+			case 2: return "pointer.button.right"
+			default: return `pointer.button.${event.button + 1}`
+		}
+	}
+
+	static wheelCodes(event: WheelEvent) {
+		const movements: [string, number][] = []
+
+		if (event.deltaX)
+			movements.push([
+				event.deltaX > 0 ? "pointer.wheel.right" : "pointer.wheel.left",
+				event.deltaX,
+			])
+
+		if (event.deltaY)
+			movements.push([
+				event.deltaY > 0 ? "pointer.wheel.down" : "pointer.wheel.up",
+				event.deltaY,
+			])
+
+		return movements
+	}
+
+	*samples() {
+		this.#specialPreProcessing()
+		yield* super.samples()
+	}
+
+	#specialPreProcessing() {
+		const [x, y] = this.movement
+		const [left, right] = splitAxis(x)
+		const [down, up] = splitAxis(y)
+
+		if (x) {
+			if (x >= 0)
+				this.setSample(`pointer.move.right`, Math.abs(right))
+			else
+				this.setSample(`pointer.move.left`, Math.abs(left))
+		}
+		if (y) {
+			if (y >= 0)
+				this.setSample(`pointer.move.up`, Math.abs(up))
+			else
+				this.setSample(`pointer.move.down`, Math.abs(down))
+		}
+
+		this.movement.set_(0, 0)
+	}
+}
+
---
+++ s/bravo/device/sampler.ts
@@ -0,0 +1,25 @@
+
+import {sub} from "@e280/stz"
+import {Device, Sample} from "../core/types.js"
+import {SampleMap} from "../core/parts/sample-map.js"
+
+export class SamplerDevice implements Device {
+	on = sub<Sample>()
+	sampleMap = new SampleMap()
+
+	setSample(code: string, value: number) {
+		this.sampleMap.set(code, value)
+		this.on.pub(code, value)
+		return this
+	}
+
+	zero() {
+		for (const code of this.sampleMap.keys())
+			this.setSample(code, 0)
+	}
+
+	*samples() {
+		yield* this.sampleMap
+	}
+}
+
