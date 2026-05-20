1a3834d3b4e37fdf91e7506065543544a6465076
Chase Moskal <chasemoskal@gmail.com>
2026-05-19 18:20:05 -0700

    remove: gamepad corrections (biased stick heading)

--- s/device/gamepad.ts
+++ s/device/gamepad.ts
@@ -1,13 +1,9 @@
 
-import {Scalar} from "@benev/math"
 import {Pad} from "./pad/pad.js"
 import {SamplerDevice} from "./sampler.js"
 import {splitAxis} from "../utils/split-axis.js"
 
 export class GamepadDevice extends SamplerDevice {
-	min = 0.2
-	max = 0.9
-
 	constructor(public pad: Pad) {
 		super()
 	}
@@ -28,22 +24,18 @@ export class GamepadDevice extends SamplerDevice {
 			const i = index + 1
 			const value = button.value
 			anyButtonValue = Math.max(anyButtonValue, value)
-			this.setSample(`gamepad.button.${i}`, this.#correction(value))
+			this.setSample(`gamepad.button.${i}`, value)
 		}
-		this.setSample("gamepad.button.any", this.#correction(anyButtonValue))
-	}
-
-	#correction(x: number, min = this.min, max = this.max) {
-		return Scalar.remap(x, min, max, 0, 1, true)
+		this.setSample("gamepad.button.any", anyButtonValue)
 	}
 
 	#pollSticks(gamepad: Gamepad) {
 		for (const [index, axis] of gamepad.axes.entries()) {
 			const i = index + 1
-			this.setSample(`gamepad.axis.${i}`, this.#correction(axis, -this.max, this.max))
+			this.setSample(`gamepad.axis.${i}`, axis)
 			const [neg, pos] = splitAxis(axis)
-			this.setSample(`gamepad.axis.${i}.neg`, this.#correction(neg))
-			this.setSample(`gamepad.axis.${i}.pos`, this.#correction(pos))
+			this.setSample(`gamepad.axis.${i}.neg`, neg)
+			this.setSample(`gamepad.axis.${i}.pos`, pos)
 		}
 	}
 }
